9e0bf24450fccf58e6f9154050a387f822b198c6
ThanhND <danhthanh3452@gmail.com>
2025-06-19 17:27:59 +0700

    feat: add mapstruct and response api

---
+++ src/main/java/me/danhthanhf/distant_class/common/response/ApiResponse.java
@@ -0,0 +1,46 @@
+package me.danhthanhf.distant_class.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@NoArgsConstructor
+public class ApiResponse <T>{
+    private String message;
+    private T data;
+    private boolean success;
+    private Object errors; // contains error details ex: validation errors, etc.
+
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+       ApiResponse<T> res = new ApiResponse<>();
+       res.setSuccess(true);
+       res.setMessage(message);
+       res.setData(data);
+       return res;
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        ApiResponse<T> res = new ApiResponse<>();
+        res.setMessage(message);
+        res.setSuccess(true);
+        return res;
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        ApiResponse<T> res = new ApiResponse<>();
+        res.setMessage(message);
+        res.setSuccess(false);
+        return res;
+    }
+
+    public static <T> ApiResponse<T> error(String message, Object errors) {
+        ApiResponse<T> res = new ApiResponse<>();
+        res.setMessage(message);
+        res.setSuccess(false);
+        res.setErrors(errors);
+        return res;
+    }
+}
---
+++ src/main/java/me/danhthanhf/distant_class/controller/AuthController.java
@@ -0,0 +1,13 @@
+package me.danhthanhf.distant_class.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.base.path}/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public
+}
