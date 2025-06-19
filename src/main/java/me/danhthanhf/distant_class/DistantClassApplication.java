38b9882b8d816f402073fcd67941427c15456f22
ThanhND <danhthanh3452@gmail.com>
2025-06-19 09:47:57 +0700

    first commit

---
+++ src/main/java/me/danhthanhf/distant_class/DistantClassApplication.java
@@ -0,0 +1,13 @@
+package me.danhthanhf.distant_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistantClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistantClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/danhthanhf/distant_class/DistantClassApplicationTests.java
@@ -0,0 +1,13 @@
+package me.danhthanhf.distant_class;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistantClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
